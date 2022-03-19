36a886f4b0d6daba1106d4c4169cfed0134d041f
Jan Mokracki <jan.mokracki@wp.pl>
2022-03-19 23:00:32 +0100

    Small fix

--- src/main/java/com/janmokrackiservletproject/servlets/WelcomeServlet.java
+++ src/main/java/com/janmokrackiservletproject/servlets/WelcomeServlet.java
@@ -37,5 +37,6 @@ public class WelcomeServlet extends HttpServlet {
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
         response.setDateHeader("Expires", 0);
+        this.doGet(request, response);
     }
 }
